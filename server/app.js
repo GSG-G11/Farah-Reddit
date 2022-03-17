6147f4638f8177a6b298f67c06c8ee92f16141ca
farah2003 <farah.shaqoura@example.com>
2022-03-17 19:16:06 +0200

    AddPORT

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const {router}=require('./router');
 const { pageNotFound ,severError} = require('./controllers');
 const app =express();
-app.set('port',process.env.port || 8080)
+app.set('port',process.env.PORT || 8080)
 app.use(compression())
 app.use(express.json())
 app.use(cookieParser())
